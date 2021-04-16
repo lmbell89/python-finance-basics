49adb28090afa36518654fd4ba9d80633072b08e
Liam Bell <l.m.bell89@gmail.com>
2021-04-16 11:56:41 +0100

    added function to determine number of payments

--- amortisation.py
+++ amortisation.py
@@ -1,5 +1,10 @@
+import math
+
 def amortise(r, n, P):
-    return P * ((r * (1 + r) ** n) / ((1 + r) ** n - 1))
+    return P * (r * (1 + r) ** n) / ((1 + r) ** n - 1)
+
+def findN(A, P, r):
+    return math.log(-A / (P * r - A)) / math.log(1 + r)
 
 def final_payment(A, n):
     rounding_error = (A * 100) % 1 * n
@@ -15,5 +20,7 @@ def schedule(r, n, P):
 rate = 0.05
 months = 5 * 12
 loan_value = 10000
+payment = 528.28
 
 print(schedule(rate, months, loan_value))
+print(findN(payment, loan_value, rate))
